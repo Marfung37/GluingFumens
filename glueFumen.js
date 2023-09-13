70e73f83c584b53e8f59527720787656c803dc0c
Marfung <marvin.fung37@gmail.com>
2023-09-13 18:07:52 -0500

    Added visualizer output

--- glueFumen.js
+++ glueFumen.js
@@ -45,7 +45,7 @@ const rotationDict = {
     3: "right"
 }
 
-function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, depth=0){
+function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
     const piece = field.at(x, y);
 
     const rotationStates = pieceMappings[piece];
@@ -56,10 +56,31 @@ function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool
     for(let state = 0; state < rotationStates.length; state++){
         let minoPositions = [];
         let newPiecesArr = piecesArr.slice();
+
+        // create fumen of trying to put piece there
+        let grayFieldStr = "";
+        for(let mino of field.str().split("\n").slice(0, -1).join("")){
+            if(mino == "X"){
+                grayFieldStr += "X";
+            } else {
+                grayFieldStr += "_";
+            }
+        }
+        let grayField = Field.create(grayFieldStr);
+        grayField.fill({
+                type: piece,
+                rotation: rotationDict[state],
+                x: x + rotationStates[state][0][0],
+                y: y + rotationStates[state][0][1]
+            }, true);
+
+        grayField = Field.create(grayField.str().split("\n").slice(-(y + 2), -1).join("")); // get only 4 lines
+
+        visualizeArr.push({field: grayField});
+
         for(let pos of rotationStates[state]){
             let posX = x + pos[0];
             let posY = y + pos[1];
-
             // checks for position is in bounds
             if(posX < 0 || posX >= rowLen){
                 break;
@@ -98,6 +119,7 @@ function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool
                 // change the field to be the piece to be replaced by gray
                 newField.set(posX, posY, "X");
             }
+
             let oldHeight = newField.str().split("\n").length - 1;
             if(removeLineClearBool){
                 newField = removeLineClears(newField);
@@ -117,7 +139,7 @@ function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool
 
             let oldLen = allPiecesArr.length;
 
-            let data = scanField(startx, starty, newField, newPiecesArr, allPiecesArr, removeLineClearBool, depth + 1)
+            let data = scanField(startx, starty, newField, newPiecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth + 1)
             let possPiecesArr = data[0];
             leftoverPieces = data[1];
 
@@ -141,13 +163,13 @@ function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool
     return [found, leftoverPieces]
 }
 
-function scanField(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, depth=0){
+function scanField(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
     var newX = x;
     for(let newY = y; newY >= 0; newY--){
         for(; newX < rowLen; newX++){
             // if it is a piece
             if(field.at(newX, newY) != "X" && field.at(newX, newY) != "_"){
-                let [rotationWorked, leftover] = checkRotation(newX, newY, field, piecesArr, allPiecesArr, removeLineClearBool, depth)
+                let [rotationWorked, leftover] = checkRotation(newX, newY, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth)
                 if(rotationWorked){
                     // a rotation works for the piece so just end the function as the scan finished
                     return [null, leftover];
@@ -201,7 +223,7 @@ function findRemainingPieces(field){
     return piecesFound;
 }
 
-function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true){
+function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true, visualize=false){
     var fumenCodes = [];
 
     if(typeof customInput != 'object'){
@@ -214,18 +236,18 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true){
 
     var allPiecesArr = [];
     var allFumens = [];
+    var visualizeArr = [];
     var fumenIssues = 0;
     for(let code of fumenCodes){
         let inputPages = decoder.decode(code);
         let thisGlueFumens = []; // holds the glue fumens for this fumenCode
         for(let pageNum = 0; pageNum < inputPages.length; pageNum++){
             let field = inputPages[pageNum].field;
-            field = removeLineClears(field);
             const height = field.str().split("\n").length - 1;
             let emptyField = makeEmptyField(field, height);
             allPiecesArr = []
 
-            scanField(0, height - 1, field, [], allPiecesArr, removeLineClearBool);
+            scanField(0, height - 1, field, [], allPiecesArr, removeLineClearBool, visualizeArr);
             
             if(allPiecesArr.length == 0){
                 console.log(code + " couldn't be glued");
@@ -243,6 +265,8 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true){
                         operation: piecesArr[i]
                     })
                 }
+                if(visualize)
+                    visualizeArr.push(...pages);
                 let pieceFumen = encoder.encode(pages);
                 thisGlueFumens.push(pieceFumen);
             }
@@ -260,12 +284,15 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true){
         console.log("Warning: " + fumenIssues + " fumens couldn't be glued");
     }
 
+    if(visualize)
+        return [encoder.encode(visualizeArr)];
+
     return allFumens
 }
 
 exports.glueFumen = glueFumen;
 
 if(require.main == module){
-    allFumens = glueFumen()
+    allFumens = glueFumen();
     console.log(allFumens.join("\n"));
 }
