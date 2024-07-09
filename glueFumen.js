13e99fbe3e1b953e81482d1d1bfe6d4c8a9766df
Marvin Fung <marvin.fung37@gmail.com>
2024-07-08 21:08:00 -0400

    Restructured code to check pieces from bottom up and remove assumption that if piece is found don't continue scaning the field

--- glueFumen.js
+++ glueFumen.js
@@ -1,40 +1,40 @@
 const {decoder, encoder, Field} = require('tetris-fumen');
 
-const rowLen = 10;
+const width = 10;
 
 const pieceMappings = {
     "T": [
-        [[0, -1], [0, 0], [-1, -1], [1, -1]],
-        [[0, -1], [0, 0], [-1, -1], [0, -2]],
-        [[1, 0], [0, 0], [2, 0], [1, -1]],
-        [[0, -1], [0, 0], [1, -1], [0, -2]]
+        [[1, 0], [0, 0], [1, 1], [2, 0]],
+        [[0, 1], [0, 0], [-1, 1], [0, 2]],
+        [[0, 1], [0, 0], [-1, 1], [1, 1]],
+        [[0, 1], [0, 0], [1, 1], [0, 2]],
         ],
     "I": [
         [[1, 0], [0, 0], [2, 0], [3, 0]],
-        [[0, -2], [0, 0], [0, -1], [0, -3]]
+        [[0, 1], [0, 0], [0, 2], [0, 3]],
         ],
     "L": [
-        [[-1, -1], [0, 0], [-2, -1], [0, -1]],
-        [[1, -1], [0, 0], [1, 0], [1, -2]],
-        [[1, 0], [0, 0], [2, 0], [0, -1]],
-        [[0, -1], [0, 0], [0, -2], [1, -2]]
+        [[1, 0], [0, 0], [2, 0], [2, 1]],
+        [[0, 1], [0, 0], [0, 2], [-1, 2]],
+        [[1, 1], [0, 0], [0, 1], [2, 1]],
+        [[0, 1], [0, 0], [1, 0], [0, 2]],
         ],
     "J": [
-        [[1, -1], [0, 0], [0, -1], [2, -1]],
-        [[0, -1], [0, 0], [-1, -2], [0, -2]],
-        [[1, 0], [0, 0], [2, 0], [2, -1]],
-        [[0, -1], [0, 0], [1, 0], [0, -2]]
+        [[1, 0], [0, 0], [0, 1], [2, 0]],
+        [[1, 1], [0, 0], [1, 0], [1, 2]],
+        [[-1, 1], [0, 0], [-2, 1], [0, 1]],
+        [[0, 1], [0, 0], [0, 2], [1, 2]]
         ],
     "S": [
-        [[0, -1], [0, 0], [1, 0], [-1, -1]],
-        [[1, -1], [0, 0], [0, -1], [1, -2]]
+        [[1, 0], [0, 0], [1, 1], [2, 1]],
+        [[0, 1], [0, 0], [-1, 1], [-1, 2]]
         ],
     "Z": [
-        [[1, -1], [0, 0], [1, 0], [2, -1]],
-        [[0, -1], [0, 0], [-1, -1], [-1, -2]]
+        [[0, 0], [1, 0], [-1, 1], [0, 1]],
+        [[1, 1], [0, 0], [0, 1], [1, 2]]
         ],
     "O": [
-        [[0, -1], [0, 0], [1, 0], [1, -1]]
+        [[0, 0], [1, 0], [0, 1], [1, 1]]
         ]
 }
 
@@ -45,165 +45,19 @@ const rotationDict = {
     3: "right"
 }
 
-function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
-    const piece = field.at(x, y);
-
-    const rotationStates = pieceMappings[piece];
-
-    let found = false;
-    let leftoverPieces = null;
-
-    for(let state = 0; state < rotationStates.length; state++){
-        let minoPositions = [];
-        let newPiecesArr = piecesArr.slice();
-
-        // create fumen of trying to put piece there
-        let grayFieldStr = "";
-        for(let mino of field.str().split("\n").slice(0, -1).join("")){
-            if(mino == "X"){
-                grayFieldStr += "X";
-            } else {
-                grayFieldStr += "_";
-            }
-        }
-        let grayField = Field.create(grayFieldStr);
-        grayField.fill({
-                type: piece,
-                rotation: rotationDict[state],
-                x: x + rotationStates[state][0][0],
-                y: y + rotationStates[state][0][1]
-            }, true);
-
-        grayField = Field.create(grayField.str().split("\n").slice(-(y + 2), -1).join("")); // get only 4 lines
-
-        visualizeArr.push({field: grayField});
-
-        for(let pos of rotationStates[state]){
-            let posX = x + pos[0];
-            let posY = y + pos[1];
-            // checks for position is in bounds
-            if(posX < 0 || posX >= rowLen){
-                break;
-            }
-            if(posY < 0){
-                break;
-            }
-
-            if(field.at(posX, posY) == piece){
-                minoPositions.push([posX, posY]);
-            }
-            else{
-                break;
-            }
-        }
-        // if there's 4 minos
-        if(minoPositions.length == 4){
-            // a rotation is found
-            let foundBefore = found;
-
-            found = true;
-
-            // a rotation that works
-            let operPiece = {
-                type: piece,
-                rotation: rotationDict[state],
-                x: minoPositions[0][0],
-                y: minoPositions[0][1]
-            }
-            newPiecesArr.push(operPiece)
-
-            let newField = field.copy()
-            for(let pos of minoPositions){
-                let posX = pos[0];
-                let posY = pos[1];
-                // change the field to be the piece to be replaced by gray
-                newField.set(posX, posY, "X");
-            }
-
-            let oldHeight = newField.str().split("\n").length - 1;
-            if(removeLineClearBool){
-                newField = removeLineClears(newField);
-            }
-            
-
-            const height = newField.str().split("\n").length - 1;
-
-            // check if a line clear occurred
-            let startx = x;
-            let starty = y
-            if(oldHeight > height){
-                // start position to 0 otherwise it's where we left off scanning the field
-                startx = 0;
-                starty = height - 1
-            }
-
-            let oldLen = allPiecesArr.length;
-
-            let data = scanField(startx, starty, newField, newPiecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth + 1)
-            let possPiecesArr = data[0];
-            leftoverPieces = data[1];
-
-            if(leftoverPieces == null){
-                leftoverPieces = findRemainingPieces(newField)
-            }
-            
-            // if the field doesn't have any more pieces it's good
-            if(possPiecesArr != null && leftoverPieces.length == 0){
-                allPiecesArr.push(possPiecesArr);
-            } else if(oldLen == allPiecesArr.length){
-                // the piece didn't result into a correct glued fumen
-                if(!leftoverPieces.includes(piece)){
-                    return [found, leftoverPieces]
-                } else {
-                    found = foundBefore
-                }
-            }
-        }
-    }
-    return [found, leftoverPieces]
+function height(field){
+    // accounting for newlines and no trailing newline and garbage line
+    return ((field.str().length + 1) / (width + 1) - 1);
 }
 
-function scanField(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
-    var newX = x;
-    for(let newY = y; newY >= 0; newY--){
-        for(; newX < rowLen; newX++){
-            // if it is a piece
-            if(field.at(newX, newY) != "X" && field.at(newX, newY) != "_"){
-                let [rotationWorked, leftover] = checkRotation(newX, newY, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth)
-                if(rotationWorked){
-                    // a rotation works for the piece so just end the function as the scan finished
-                    return [null, leftover];
-                }
-                // skips this one that meets no rotation as it might be a cut piece
-            }
-        }
-        newX = 0
-    }
-    return [piecesArr, null];
+function isInside(field, x, y){
+    return 0 <= x && x < width && 0 <= y < height(field);
 }
 
-function makeEmptyField(field, height){
-    var emptyField = field.copy();
-    for(let y = height-1; y >= 0; y--){
-        for(let x = 0; x < rowLen; x++){
-            let piece = emptyField.at(x, y);
-            if(piece.match(/[TILJSZO]/)){
-                emptyField.set(x, y, "_");
-            }
-        }
+function placePiece(field, minoPositions){
+    for (let [x, y] of minoPositions){
+        field.set(x, y, "X")
     }
-    return emptyField;
-}
-
-function removeLineClears(field){
-    var lines = field.str().split("\n");
-    for(let i = lines.length-1; i >= 0; i--){
-        if(lines[i].match(/X{10}/)){
-            lines.splice(i, 1);
-        }
-    }
-    const newField = Field.create(lines.slice(0, -1).join(""), lines[-1]);
-    return newField;
 }
 
 function findRemainingPieces(field){
@@ -223,11 +77,142 @@ function findRemainingPieces(field){
     return piecesFound;
 }
 
+function removeLineClears(field){
+    var lines = field.str().split("\n");
+    for(let i = lines.length-1; i >= 0; i--){
+        if(lines[i].match(/X{10}/)){
+            lines.splice(i, 1);
+        }
+    }
+    const newField = Field.create(lines.slice(0, -1).join(""), lines[-1]);
+    return newField;
+}
+
+function checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
+    const piece = field.at(x, y);
+
+    const rotationStates = pieceMappings[piece];
+
+    let found = false;
+    let leftoverPieces = null;
+
+    for(let state = 0; state < rotationStates.length; state++){
+        let minoPositions = [];
+        let newPiecesArr = piecesArr.slice();
+
+        // create fumen of trying to put piece there
+        let visualizeField = makeEmptyField(field)
+        for(let pos of rotationStates[state]){
+            let px = x + pos[0];
+            let py = y + pos[1];
+            
+            if(isInside(field, px, py)) {
+                visualizeField.set(px, py, piece);
+                if(field.at(px, py) == piece) {
+                    minoPositions.push([px, py])
+                }
+            }
+        }
+        visualizeArr.push({field: visualizeField});
+
+        // if there's 4 minos
+        if(minoPositions.length == 4){
+            // a rotation is found
+            let foundBefore = found;
+            found = true;
+
+            // a rotation that works
+            let operPiece = {
+                type: piece,
+                rotation: rotationDict[state],
+                x: minoPositions[0][0],
+                y: minoPositions[0][1]
+            }
+            newPiecesArr.push(operPiece)
+
+            let newField = field.copy()
+            placePiece(newField, minoPositions);
+
+            let oldHeight = height(newField);
+            if(removeLineClearBool){
+                newField = removeLineClears(newField);
+            }
+
+
+            // check if a line clear occurred
+            let startx = x;
+            let starty = y;
+            if(oldHeight > height(newField)){
+                // start position to 0 otherwise it's where we left off scanning the field
+                startx = 0;
+                starty = 0;
+            }
+
+            let oldLen = allPiecesArr.length;
+
+            let possPiecesArr = scanField(
+              startx, 
+              starty, 
+              newField, 
+              newPiecesArr, 
+              allPiecesArr, 
+              removeLineClearBool, 
+              visualizeArr, 
+              depth + 1
+            )
+
+            leftoverPieces = findRemainingPieces(newField)
+            
+            // if the field doesn't have any more pieces it's good
+            if(possPiecesArr != null && leftoverPieces.length == 0){
+                allPiecesArr.push(possPiecesArr);
+            } else if(oldLen == allPiecesArr.length){
+                // the piece didn't result into a correct glued fumen
+                if(!leftoverPieces.includes(piece)){
+                    return leftoverPieces
+                } else {
+                    found = foundBefore
+                }
+            }
+        }
+    }
+    return found
+}
+
+// scan until find next colored mino to run checkRotation on it
+function scanField(x0, y0, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth=0){
+    const fieldHeight = height(field);
+    for(let y = y0; y < fieldHeight; y++){
+        for(let x = (y == y0) ? x0 : 0; x < width; x++){
+            // if it is a piece
+            if(field.at(x, y).match(/[TILJSZO]/)){
+                checkRotation(x, y, field, piecesArr, allPiecesArr, removeLineClearBool, visualizeArr, depth)
+                // continue on with possiblity this is piece is cut
+            }
+        }
+    }
+    return piecesArr;
+}
+
+function makeEmptyField(field){
+    var emptyField = field.copy();
+    const fieldHeight = height(field);
+    for(let y = 0; y < fieldHeight; y++){
+        for(let x = 0; x < width; x++){
+            let piece = emptyField.at(x, y);
+            if(piece.match(/[TILJSZO]/)){
+                emptyField.set(x, y, "_");
+            }
+        }
+    }
+    return emptyField;
+}
+
 function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true, visualize=false){
     var fumenCodes = [];
 
-    if(typeof customInput != 'object'){
-        throw new Error("Custom input for glueFumen must be an array")
+    if(!Array.isArray(customInput)){
+        customInput = [customInput];
     }
 
     for(let rawInput of customInput){
@@ -243,11 +228,10 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true, 
         let thisGlueFumens = []; // holds the glue fumens for this fumenCode
         for(let pageNum = 0; pageNum < inputPages.length; pageNum++){
             let field = inputPages[pageNum].field;
-            const height = field.str().split("\n").length - 1;
             let emptyField = makeEmptyField(field, height);
             allPiecesArr = []
 
-            scanField(0, height - 1, field, [], allPiecesArr, removeLineClearBool, visualizeArr);
+            scanField(0, 0, field, [], allPiecesArr, removeLineClearBool, visualizeArr);
             
             if(allPiecesArr.length == 0){
                 console.log(code + " couldn't be glued");
@@ -273,7 +257,7 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true, 
 
             if(allPiecesArr.length > 1){
                 // multiple outputs warning
-                allFumens.push("Warning: " + code + " led to " + allPiecesArr.length + " outputs: " + thisGlueFumens.join(" "));
+                allFumens.push("Warning: " + code + " led to " + allPiecesArr.length + " outputs");
             }
         }
 
@@ -293,6 +277,6 @@ function glueFumen(customInput=process.argv.slice(2), removeLineClearBool=true, 
 exports.glueFumen = glueFumen;
 
 if(require.main == module){
-    allFumens = glueFumen();
+    allFumens = glueFumen("v115@fgi0G8whi0F8whi0F8whi0F8whi0F8h0H8i0G8JeAg?WGAqPNPCq/AAA", true, false);
     console.log(allFumens.join("\n"));
 }
